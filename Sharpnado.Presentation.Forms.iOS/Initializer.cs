d587b1a5b85198107fb82ced8f6d2c1bf108be9d
Roubachof <jm.alfonsi@gmail.com>
2018-08-30 19:21:38 +0200

    Creation of HorizontalListView sln for list view only nuget package

--- Sharpnado.Presentation.Forms.iOS/Initializer.cs
+++ Sharpnado.Presentation.Forms.iOS/Initializer.cs
@@ -1,9 +1,12 @@
-﻿namespace Sharpnado.Presentation.Forms.iOS
+﻿using Sharpnado.Presentation.Forms.iOS.Renderers.HorizontalList;
+
+namespace Sharpnado.Presentation.Forms.iOS
 {
     public static class SharpnadoInitializer
     {
         public static void Initialize()
         {
+            iOSHorizontalListViewRenderer.Initialize();
         }
     }
 }
