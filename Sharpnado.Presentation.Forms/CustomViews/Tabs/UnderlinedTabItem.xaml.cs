6ecb09f5bd3b94c540afcfcebabfc2429bea68fe
roubachof <jm.alfonsi@gmail.com>
2019-09-05 15:19:56 +0200

    add UnderlineAllTab property

--- Sharpnado.Presentation.Forms/CustomViews/Tabs/UnderlinedTabItem.xaml.cs
+++ Sharpnado.Presentation.Forms/CustomViews/Tabs/UnderlinedTabItem.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Sharpnado.Presentation.Forms.CustomViews.Tabs
@@ -6,11 +7,67 @@ namespace Sharpnado.Presentation.Forms.CustomViews.Tabs
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UnderlinedTabItem : TabItem
     {
+        public static readonly BindableProperty UnderlineAllTabProperty = BindableProperty.Create(
+            nameof(UnderlineAllTab),
+            typeof(bool),
+            typeof(TabItem),
+            true);
+
         public UnderlinedTabItem()
         {
             LabelSize = 14;
 
             InitializeComponent();
+        }
+
+        public bool UnderlineAllTab
+        {
+            get => (bool)GetValue(UnderlineAllTabProperty);
+            set => SetValue(UnderlineAllTabProperty, value);
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            switch (propertyName)
+            {
+                case nameof(Margin):
+                    if (UnderlineAllTab)
+                    {
+                        Underline.Margin = new Thickness(Underline.Margin.Left - Margin.Left, 0, Underline.Margin.Right - Margin.Right, 0);
+                    }
+                    else
+                    {
+                        Underline.Margin = new Thickness(0);
+                    }
+
+                    break;
+
+                case nameof(Padding):
+                    if (UnderlineAllTab)
+                    {
+                        Underline.Margin = new Thickness(Underline.Margin.Left - Padding.Left, 0, Underline.Margin.Right - Padding.Right, 0);
+                    }
+                    else
+                    {
+                        Underline.Margin = new Thickness(0);
+                    }
+
+                    break;
+
+                case nameof(UnderlineAllTab):
+                    if (UnderlineAllTab)
+                    {
+                        Underline.Margin = new Thickness(-Margin.Left - Padding.Left, 0, -Margin.Right - Padding.Right, 0);
+                    }
+                    else
+                    {
+                        Underline.Margin = new Thickness(0);
+                    }
+
+                    break;
+            }
         }
     }
 }
