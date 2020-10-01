97eef971ad5bde01697d67babd79fbf331497471
jmmontero <jmmontero.ortega@gmail.com>
2020-10-01 18:20:48 +0200

    #129 Include a little sample to test issue 129

---
+++ Sharpnado.DragAndDropCollection.Sample/Sharpnado.DragAndDropCollection.Sample/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sharpnado.DragAndDropCollection.Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        protected override void OnAppearing()
+            => (this.BindingContext as MainViewModel).OnStartCommand.Execute(null);
+    }
+}
